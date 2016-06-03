a279324ccca7d93644542f3f6749124451baafd9
Nathan Thompson <thompsnm@gmail.com>
2016-06-03 16:42:19 -0600

    Ran `swagger project generate-test`

---
+++ restaurant-service/test/api/client/hello-test.js
@@ -0,0 +1,69 @@
+'use strict';
+var chai = require('chai');
+var ZSchema = require('z-schema');
+var validator = new ZSchema({});
+var supertest = require('supertest');
+var api = supertest('http://localhost:10010'); // supertest init;
+var expect = chai.expect;
+
+describe('/hello', function() {
+  describe('get', function() {
+    it('should respond with 200 Success', function(done) {
+      /*eslint-disable*/
+      var schema = {
+        "required": [
+          "message"
+        ],
+        "properties": {
+          "message": {
+            "type": "string"
+          }
+        }
+      };
+
+      /*eslint-enable*/
+      api.get('/hello')
+      .query({
+name: 'DATA GOES HERE'
+      })
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+
+        expect(validator.validate(res.body, schema)).to.be.true;
+        done();
+      });
+    });
+
+    it('should respond with default Error', function(done) {
+      /*eslint-disable*/
+      var schema = {
+        "required": [
+          "message"
+        ],
+        "properties": {
+          "message": {
+            "type": "string"
+          }
+        }
+      };
+
+      /*eslint-enable*/
+      api.get('/hello')
+      .query({
+name: 'DATA GOES HERE'
+      })
+      .set('Accept', 'application/json')
+      .expect('DEFAULT RESPONSE CODE HERE')
+      .end(function(err, res) {
+        if (err) return done(err);
+
+        expect(validator.validate(res.body, schema)).to.be.true;
+        done();
+      });
+    });
+
+  });
+
+});
---
+++ restaurant-service/test/api/client/restaurant-test.js
@@ -0,0 +1,73 @@
+'use strict';
+var chai = require('chai');
+var ZSchema = require('z-schema');
+var validator = new ZSchema({});
+var supertest = require('supertest');
+var api = supertest('http://localhost:10010'); // supertest init;
+var expect = chai.expect;
+
+describe('/restaurant', function() {
+  describe('get', function() {
+    it('should respond with 200 Success', function(done) {
+      /*eslint-disable*/
+      var schema = {
+        "required": [
+          "name",
+          "type"
+        ],
+        "properties": {
+          "name": {
+            "type": "string"
+          },
+          "type": {
+            "type": "string"
+          }
+        }
+      };
+
+      /*eslint-enable*/
+      api.get('/restaurant')
+      .query({
+name: 'DATA GOES HERE'
+      })
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+
+        expect(validator.validate(res.body, schema)).to.be.true;
+        done();
+      });
+    });
+
+    it('should respond with default Error', function(done) {
+      /*eslint-disable*/
+      var schema = {
+        "required": [
+          "message"
+        ],
+        "properties": {
+          "message": {
+            "type": "string"
+          }
+        }
+      };
+
+      /*eslint-enable*/
+      api.get('/restaurant')
+      .query({
+name: 'DATA GOES HERE'
+      })
+      .set('Accept', 'application/json')
+      .expect('DEFAULT RESPONSE CODE HERE')
+      .end(function(err, res) {
+        if (err) return done(err);
+
+        expect(validator.validate(res.body, schema)).to.be.true;
+        done();
+      });
+    });
+
+  });
+
+});
