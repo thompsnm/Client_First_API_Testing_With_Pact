0e74bd050162937b68f6e3a742d0cfed6a3ae5a0
Nathan Thompson <thompsnm@gmail.com>
2016-06-05 10:18:05 -0600

    Updates test to match resstaurant-service endpoint

--- restaurant-type-consumer/test/restaurant-service.pact-consumer-js-dsl.spec.js
+++ restaurant-type-consumer/test/restaurant-service.pact-consumer-js-dsl.spec.js
@@ -3,17 +3,16 @@ var chai = require('chai');
 var expect = chai.expect;
 
 describe("pact-consumer-js-dsl", function() {
-  var client, helloProvider;
+  var client, restaurantTypeProvider;
   var Pact = require('../bower_components/pact-consumer-js-dsl/dist/pact-consumer-js-dsl.js')
 
   before(function() {
     //ProviderClient is the class you have written to make the HTTP calls to the provider
     //client = new ProviderClient('http://localhost:1234');
-    helloProvider = Pact.mockService({
-      consumer: 'Hello Consumer',
-      provider: 'Hello Provider',
+    restaurantTypeProvider = Pact.mockService({
+      consumer: 'Restaurant Type Consumer',
+      provider: 'Restaurant Service',
       port: 1234,
-      dir: 'pacts',
       done: function (error) {
         expect(error).to.equal(null);
       }
@@ -21,25 +20,27 @@ describe("pact-consumer-js-dsl", function() {
 
     // This ensures your pact-mock-service is in a clean state before
     // running your test suite.
-    helloProvider.resetSession();
+    restaurantTypeProvider.resetSession();
   });
 
   it("should say hello", function(done) {
-    helloProvider
-      .given("an alligator with the name Mary exists")
-      .uponReceiving("a request for an alligator")
-      .withRequest("get", "/alligators/Mary")
+    restaurantTypeProvider
+      .given("a set of restaurant types exists")
+      .uponReceiving("a request for all restaurant types")
+      .withRequest("get", "/types")
       .willRespondWith(200, {
         "Content-Type": "application/json"
       }, {
-        "name": "Mary"
+        "type": ["Italian", "Comfort Food"]
       });
 
-    helloProvider.run(done, function(runComplete) {
+    restaurantTypeProvider.run(done, function(runComplete) {
       request
-        .get('http://localhost:1234/alligators/Mary')
+        .get('http://localhost:1234/types')
         .end(function(err, res){
-          console.log(res.body);
+          expect(res.body).to.deep.equal({
+            "type": ["Italian", "Comfort Food"]
+          });
           runComplete();
         });
     });
