abc15652ba4b0740de8195cdf8458a6e0871ad70
Nathan Thompson <thompsnm@gmail.com>
2016-06-09 02:08:18 -0600

    Rev version on rating consumer pact

--- restaurant-rating-consumer/pact-publisher.js
+++ restaurant-rating-consumer/pact-publisher.js
@@ -3,7 +3,7 @@ var pact = require('@pact-foundation/pact-node');
 var opts = {
     pactUrls: ['./tmp/pacts'],               // Array of local Pact files or directories containing them. Required.
     pactBroker: 'http://pactbroker-1.8de890fb.cont.dockerapp.io',            // URL to fetch the provider states for the given provider API. Optional.
-    consumerVersion: '1.0.4'
+    consumerVersion: '1.0.5'
 };
 
 console.log('Attempting to publish pacts to ' + opts.pactBroker);
