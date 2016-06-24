18d737c887439dff53dd4fb53c1be59332ef2416
Nathan Thompson <thompsnm@gmail.com>
2016-06-24 17:59:15 -0600

    Start using pact-node package for spinning up Pact Mock Service

--- restaurant-rating-consumer/test/restaurant-service.pact-consumer-js-dsl.spec.js
+++ restaurant-rating-consumer/test/restaurant-service.pact-consumer-js-dsl.spec.js
@@ -1,13 +1,27 @@
+var pact = require('@pact-foundation/pact-node');
 var request = require('superagent');
 var chai = require('chai');
 var expect = chai.expect;
 
 describe("Restaurant service", function() {
-  var client, restaurantTypeProvider;
+  var client, restaurantTypeProvider, server;
   var Pact = require('../bower_components/pact-consumer-js-dsl/dist/pact-consumer-js-dsl.js')
 
   var somethingLike = Pact.Match.somethingLike;
   var eachLike = Pact.Match.eachLike;
+
+  this.timeout(10000);
+
+  before(function(done) {
+    var server = pact.createServer({
+      port: 1234,            // Port number that the server runs on, defaults to 1234
+      log: 'log/pact.logs',  // File to log output on relative to current working directory, defaults to none
+      dir: 'tmp/pacts'       // Directory to write the pact contracts relative to the current working directory, defaults to none
+    });
+    server.start().then(function() {
+      done();
+    });
+  });
 
   before(function() {
     restaurantTypeProvider = Pact.mockService({
@@ -22,6 +36,10 @@ describe("Restaurant service", function() {
     // This ensures your pact-mock-service is in a clean state before
     // running your test suite.
     restaurantTypeProvider.resetSession();
+  });
+
+  after(function() {
+    pact.removeAllServers();
   });
 
   it("restaurant endpoint", function(done) {
