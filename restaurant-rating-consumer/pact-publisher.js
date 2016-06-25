ade31003b16b18d9ef9d513933b3f97eca5ea747
Nathan Thompson <thompsnm@gmail.com>
2016-06-24 18:26:49 -0600

    Fix publish script

--- restaurant-rating-consumer/pact-publisher.js
+++ restaurant-rating-consumer/pact-publisher.js
@@ -1,7 +1,7 @@
 var pact = require('@pact-foundation/pact-node');
 
 var opts = {
-    pactUrls: ['./tmp/pacts'],               // Array of local Pact files or directories containing them. Required.
+    pactUrls: ['./pacts'],               // Array of local Pact files or directories containing them. Required.
     pactBroker: 'http://pactbroker-1.8de890fb.cont.dockerapp.io',            // URL to fetch the provider states for the given provider API. Optional.
     consumerVersion: '1.0.5'
 };
